cb440c9777eec662a84ea46e52e27a1a49c4729b
Giwrgos <gkiriakos92@gmail.com>
2026-07-02 14:14:37 +0000

    Keys

--- frontend/src/config/firebase.ts
+++ frontend/src/config/firebase.ts
@@ -3,12 +3,14 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY ?? "YOUR_API_KEY",
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN ?? "YOUR_AUTH_DOMAIN",
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID ?? "YOUR_PROJECT_ID",
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET ?? "YOUR_STORAGE_BUCKET",
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ?? "YOUR_MESSAGING_SENDER_ID",
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID ?? "YOUR_APP_ID",
+  
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY ?? "AIzaSyCW3slt0r-STJr5-EKFLozoNx8wJEfyRUM",
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN ?? "campustay-da4b5.firebaseapp.com",
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID ?? "campustay-da4b5",
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET ?? "campustay-da4b5.firebasestorage.app",
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ?? "311068327323",
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID ?? "1:311068327323:web:3e9ecbd42acedd39f1353e",
+  measurementId: "G-D9ZH9Z7QMQ",
 };
 
 const app = initializeApp(firebaseConfig);
